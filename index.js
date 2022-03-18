46d18aa47e20abe96cde44f9ddc1e2a784236b45
Franklin Benjamin <franciscovibes14@gmail.com>
2022-03-18 02:43:12 -0400

    commit tree 2a106e62038a54c99460e386a6caf22726ec24bc

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+let result
+if(someValue <= 400) {
+  return 'This one is on me!'
+ } 
+ else if(someValue >= 2500) {
+   return 'No can do.'
+ } 
+ else if(someValue > 2000) 
+   return 'I will gladly take your thirty bucks.'
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(someCity){
   // Write your code here!
+  return someCity === 'NYC'? 'Ok, sounds good.':'No go.'
 }
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipAmount){
   // Write your code here!
-}
+  switch(tipAmount){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+   }
+  }
